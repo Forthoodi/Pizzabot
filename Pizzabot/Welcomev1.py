f8faed26d769b6215b10e134d9e9bdee330f8f21
Forthoodi <99222935+Forthoodi@users.noreply.github.com>
2022-02-16 09:19:43 +1300

    Creating component file
    Creating Welcome Message With random names

---
+++ Pizzabot/Welcomev1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#list of names
+names = [ "Mikara", "Louis" , "Jodek " , "Karlo " ,"Tomas", "James" , "Marcus" , "Jan" , "Tomas" ]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print ("*** Welcome to Pizza***")
+print("***My name is",name,"***")
+print("Here to help Order now !!! please dont waste my time  ")
